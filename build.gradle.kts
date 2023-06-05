0da935533aba7071ed9abacfbbb01eea2d026f43
IndianMax03 <vermamanu@mail.ru>
2023-06-05 13:33:05 +0300

    Changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,5 +17,178 @@ repositories {
 }
 
 dependencies {
+    implementation("org.postgresql:postgresql:42.4.0")
+    implementation("com.sun.faces:jsf-api:2.2.20")
+    implementation("com.sun.faces:jsf-impl:2.2.20")
+    implementation("org.primefaces:primefaces:12.0.0")
+    implementation("net.bootsfaces:bootsfaces:1.5.0")
+    implementation("org.eclipse.persistence:eclipselink:3.0.3")
+    implementation("jakarta.platform:jakarta.jakartaee-api:8.0.0")
+    testImplementation("junit:junit:4.13.2")
 }
 
+/* Compile start */
+
+tasks.compileJava {
+    options.compilerArgs.add("-Xlint:unchecked")
+}
+
+val compile = tasks.register("compile") {
+    group = project.property("tasksGroup").toString()
+    dependsOn(tasks.compileJava, tasks.compileTestJava)
+}
+
+/* Compile end */
+
+/* Jar start */
+
+val build = tasks.register<Jar>("l3build") {
+    dependsOn(compile)
+    group = project.property("tasksGroup").toString()
+    from(project.property("classesDirectory"))
+    destinationDirectory.set(layout.buildDirectory.dir(project.property("libsDirectory").toString()))
+    archiveBaseName.set(project.name)
+}
+
+/* Jar end */
+
+/* Music start */
+
+abstract class Music : AbstractExecTask<Music>(Music::class.java) {
+
+    init {
+        group = project.property("tasksGroup").toString()
+    }
+
+    override fun exec() {
+        executable = "afplay"
+
+        args(project.property("mp3File"))
+
+        super.exec()
+    }
+
+}
+
+
+val music = tasks.register<Music>("music") {
+    dependsOn(build)
+}
+
+/* Music end */
+
+/* Scp start */
+
+open class Scp : AbstractExecTask<Scp>(Scp::class.java) {
+
+    init {
+        group = project.property("tasksGroup").toString()
+    }
+
+    override fun exec() {
+        executable = "scp"
+
+        args(listOf("-P", "2222", "build/libs/WebLab3-1.0-SNAPSHOT.jar", "s333057@helios.se.ifmo.ru:/home/studs/s333057/mispi-lab3"))
+
+        super.exec()
+    }
+
+}
+
+val scp = tasks.register<Scp>("scp") {
+    dependsOn(build)
+}
+
+/* Scp end */
+
+/* XML start */
+
+abstract class Xml : AbstractExecTask<Xml>(Xml::class.java) {
+
+    @get:SkipWhenEmpty
+    @get:InputDirectory
+    abstract val sourceDirectory: DirectoryProperty
+
+    init {
+        group = project.property("tasksGroup").toString()
+    }
+
+    override fun exec() {
+        val files = sourceDirectory.asFileTree.map { it.path }
+        val xmlFiles = mutableListOf<String>()
+        val reg = """.*\.x\w*ml$""".toRegex()
+
+        files.forEach {
+            if (it.matches(reg)) {
+                xmlFiles.add(it)
+            }
+        }
+
+        var failures: Set<String> = mutableSetOf()
+        val parser: DocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+        val validator: Validator? = null
+        xmlFiles.forEach {
+            var document: Document? = null
+            try {
+                document = parser.parse(it)
+            } catch (e: Exception) {
+                logger.error("Error while parsing $it: ${e.message}")
+                failures = failures.plus(it)
+            }
+            if (document != null) {
+                try {
+                    validator?.validate(DOMSource(document))
+                } catch (e: Exception) {
+                    logger.error("Error while validating $it ${e.message}")
+                    failures = failures.plus(it)
+                }
+            }
+        }
+        if (failures.isNotEmpty()) throw BuildException("xml validation failures $failures")
+    }
+
+}
+
+val xml = tasks.register<Xml>("xml") {
+    sourceDirectory.set(layout.projectDirectory)
+}
+
+/* XML end */
+
+/* test start */
+
+tasks.test {
+    group = project.property("tasksGroup").toString()
+    project.setProperty("testResultsDirName", "$buildDir/${project.property("testOutputDir").toString()}")
+    useJUnit()
+    dependsOn(build)
+    reports {
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("${project.property("testOutputDir").toString()}/${project.property("testHtmlOutputDir").toString()}"))
+        junitXml.outputLocation.set(layout.buildDirectory.dir("${project.property("testOutputDir").toString()}/${project.property("testXmlOutputDir").toString()}"))
+    }
+}
+
+/* test end */
+
+/* report start */
+
+tasks.register<TestReport>("report") {
+    group = project.property("tasksGroup").toString()
+    dependsOn(tasks.test)
+    //  todo сделать коммит
+    exec {
+        executable = "git"
+        args("status")
+    }
+    exec {
+        executable = "echo"
+        args("*******************************************")
+    }
+    exec {
+        executable = "git"
+        args("help")
+    }
+}
+
+/* test end */
